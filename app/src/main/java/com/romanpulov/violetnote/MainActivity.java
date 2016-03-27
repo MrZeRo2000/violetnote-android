17f3ce921b481071441eaf4ee683c72653d557c8
MrZeRo2000 <MrZeRo2000@mail.ru>
2016-03-27 13:04:22 +0300

    password input

--- app/src/main/java/com/romanpulov/violetnote/MainActivity.java
+++ app/src/main/java/com/romanpulov/violetnote/MainActivity.java
@@ -1,17 +1,88 @@
 package com.romanpulov.violetnote;
 
+import android.content.DialogInterface;
+import android.os.Environment;
+import android.os.PersistableBundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.romanpulov.violetnotecore.Model.Model1;
+import com.romanpulov.violetnotecore.Model.*;
+
+import com.romanpulov.violetnotecore.AESCrypt.*;
+import com.romanpulov.violetnotecore.Processor.XMLPassDataReader;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mPassword;
+
+    private void resetPassword() {
+        mPassword = null;
+    }
+
+    private void requestPassword() {
+        if (mPassword == null) {
+            PasswordInputDialog passwordInputDialog = new PasswordInputDialog(this);
+            passwordInputDialog.setOnPasswordInputListener(new PasswordInputDialog.OnPasswordInputListener() {
+                @Override
+                public void onPasswordInput(String password) {
+                    if (password == null) {
+                        finish();
+                    }
+                    mPassword = password;
+                    Toast.makeText(getApplicationContext(), "Password: " + password, Toast.LENGTH_SHORT).show();
+                }
+            });
+            passwordInputDialog.show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestPassword();
+
+        /*
         setContentView(R.layout.activity_main);
-        Model1 model = new Model1();
-        model.method1();
+        try { 
+            AESCryptService service = new AESCryptService();
+            InputStream input =service.generateCryptInputStream(new FileInputStream(Environment.getExternalStorageDirectory() + "/temp/1.vnf"), "Password1");
+            PassData pd = (new XMLPassDataReader()).readStream(input);
+            Toast.makeText(getApplicationContext(), pd.getPassCategoryList().toString() + " - " + pd.getPassNoteList().toString(), Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        resetPassword();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        requestPassword();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestPassword();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        resetPassword();
     }
 }
---
+++ app/src/main/java/com/romanpulov/violetnote/PasswordInputDialog.java
@@ -0,0 +1,59 @@
+package com.romanpulov.violetnote;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by rpulov on 27.03.2016.
+ */
+public class PasswordInputDialog {
+
+    interface OnPasswordInputListener {
+        void onPasswordInput(String password);
+    }
+
+    private final Context mContext;
+
+    private OnPasswordInputListener mOnPasswordInputListener;
+
+    public void setOnPasswordInputListener(OnPasswordInputListener listener) {
+        this.mOnPasswordInputListener = listener;
+    }
+
+    public PasswordInputDialog(Context context) {
+        mContext = context;
+    }
+
+    public void show() {
+        final AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+        final EditText input = new EditText(mContext);
+
+        input.setInputType(InputType.TYPE_CLASS_TEXT| InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+        alert.setView(input);
+        alert.setTitle(R.string.password_required);
+
+        alert.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String password = input.getText().toString();
+                if (mOnPasswordInputListener != null)
+                    mOnPasswordInputListener.onPasswordInput(password);
+            }
+        });
+
+        alert.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (mOnPasswordInputListener != null)
+                    mOnPasswordInputListener.onPasswordInput(null);
+            }
+        });
+
+        alert.show();
+    }
+}
