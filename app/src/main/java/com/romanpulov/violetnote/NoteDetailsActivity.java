fe0be6bc3da08f253fd7fe2d9b3ff20da2d7cb43
MrZeRo2000 <MrZeRo2000@mail.ru>
2016-05-16 15:31:01 +0300

    added NoteDetails activity and fragment

---
+++ app/src/main/java/com/romanpulov/violetnote/NoteDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.romanpulov.violetnote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoteDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_note_details);
+    }
+}
