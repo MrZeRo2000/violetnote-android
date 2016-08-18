407f36301fb521d4e6ddbab59bab7909abc8b7d0
MrZeRo2000 <MrZeRo2000@mail.ru>
2016-08-18 16:18:23 +0300

    added BasicNoteActivityAdapter

--- app/src/main/java/com/romanpulov/violetnote/view/BasicNoteActivityFragment.java
+++ app/src/main/java/com/romanpulov/violetnote/view/BasicNoteActivityFragment.java
@@ -1,7 +1,10 @@
 package com.romanpulov.violetnote.view;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +22,13 @@ public class BasicNoteActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_basic_note, container, false);
+        View view = inflater.inflate(R.layout.fragment_basic_note_list, container, false);
+        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.list);
+
+        // Set the adapter
+        Context context = view.getContext();
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/romanpulov/violetnote/view/BasicNoteRecycleViewAdapter.java
@@ -0,0 +1,60 @@
+package com.romanpulov.violetnote.view;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.romanpulov.violetnote.R;
+import com.romanpulov.violetnote.model.BasicNoteA;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by romanpulov on 18.08.2016.
+ */
+public class BasicNoteRecycleViewAdapter extends RecyclerView.Adapter<BasicNoteRecycleViewAdapter.ViewHolder> {
+    private  final List<BasicNoteA> mItems;
+
+    public BasicNoteRecycleViewAdapter(List<BasicNoteA> items) {
+        mItems = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_basic_note, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTitle.setText(mItems.get(position).getTitle());
+        if (!mItems.get(position).getIsEncrypted())
+            holder.mEncryptedImage.setVisibility(View.GONE);
+        DateFormat.getDateInstance().format(new Date(mItems.get(position).getLastModified()));
+        //holder.mLastModified.setText(mItems.get(position).getLastModified());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mTitle;
+        public final TextView mLastModified;
+        public final ImageView mEncryptedImage;
+
+        public ViewHolder(View view) {
+            super(view);
+            mTitle = (TextView) view.findViewById(R.id.title);
+            mLastModified = (TextView) view.findViewById(R.id.last_modified);
+            mEncryptedImage = (ImageView) view.findViewById(R.id.encrypted_image);
+        }
+    }
+}
