18e3bed3f0cadede5f7fe28cbaafa44ffb882be1
MrZeRo2000 <MrZeRo2000@mail.ru>
2016-09-02 15:26:41 +0300

    Added base class for BaseNotePasswordActivity

---
+++ app/src/main/java/com/romanpulov/violetnote/view/core/BasicNoteDataPasswordActivity.java
@@ -0,0 +1,26 @@
+package com.romanpulov.violetnote.view.core;
+
+import android.os.Bundle;
+
+import com.romanpulov.violetnote.model.BasicNoteDataA;
+
+/**
+ * Created by romanpulov on 02.09.2016.
+ */
+public abstract class BasicNoteDataPasswordActivity extends PasswordActivity {
+    protected BasicNoteDataA mBasicNoteData;
+
+    @Override
+    protected void updatePassword(String password) {
+        // TODO: 02.09.2016 actions after password updated
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (mPasswordProvider instanceof BasicNoteDataA) {
+            mBasicNoteData = (BasicNoteDataA)mPasswordProvider;
+        }
+    }
+}
