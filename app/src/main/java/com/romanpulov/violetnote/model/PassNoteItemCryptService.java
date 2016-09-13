52197aa412acc5aa3c9ad1daebecea0e53c61d66
MrZeRo2000 <MrZeRo2000@mail.ru>
2016-09-13 14:54:06 +0300

    added procedures for crypt/decrypt items

--- app/src/main/java/com/romanpulov/violetnote/model/PassNoteItemCryptService.java
+++ app/src/main/java/com/romanpulov/violetnote/model/PassNoteItemCryptService.java
@@ -14,4 +14,33 @@ public class PassNoteItemCryptService {
             return false;
         }
     }
+
+    public static boolean encryptBasicNoteNamedItem(BasicNoteItemA item, String password) {
+        try {
+            item.setName(AESCryptService.encryptString(item.getName(), password));
+            item.setValue(AESCryptService.encryptString(item.getValue(), password));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean decryptBasicNoteCheckedItem(BasicNoteItemA item, String password) {
+        try {
+            item.setValue(AESCryptService.decryptString(item.getValue(), password));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean decryptBasicNoteNamedItem(BasicNoteItemA item, String password) {
+        try {
+            item.setName(AESCryptService.decryptString(item.getName(), password));
+            item.setValue(AESCryptService.decryptString(item.getValue(), password));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
