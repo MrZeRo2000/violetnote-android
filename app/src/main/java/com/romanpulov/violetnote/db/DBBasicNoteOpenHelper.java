2ba9bdd9b2fbdec49b9c3f7d2055df24c8b7618a
MrZeRo2000 <MrZeRo2000@mail.ru>
2017-08-08 17:36:10 +0300

    moved Priority column name to constant

--- app/src/main/java/com/romanpulov/violetnote/db/DBBasicNoteOpenHelper.java
+++ app/src/main/java/com/romanpulov/violetnote/db/DBBasicNoteOpenHelper.java
@@ -17,6 +17,7 @@ public class DBBasicNoteOpenHelper extends SQLiteOpenHelper {
     public static final String LAST_MODIFIED_COLUMN_NAME = "last_modified";
     public static final String ORDER_COLUMN_NAME = "order_id";
     public static final String VALUE_COLUMN_NAME = "value";
+    public static final String PRIORITY_COLUMN_NAME = "priority";
 
     //notes
     public static final String NOTES_TABLE_NAME = "notes";
@@ -51,7 +52,7 @@ public class DBBasicNoteOpenHelper extends SQLiteOpenHelper {
             "name",
             VALUE_COLUMN_NAME,
             "checked",
-            "priority"
+            PRIORITY_COLUMN_NAME
     };
     private static final String NOTE_ITEMS_TABLE_CREATE =
             "CREATE TABLE " + NOTE_ITEMS_TABLE_NAME + " (" +
