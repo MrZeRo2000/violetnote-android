752ae9782732b1a21f4a8fd5264cab0205022627
MrZeRo2000 <MrZeRo2000@mail.ru>
2017-08-09 16:39:22 +0300

    Order priority fixed and tests

--- app/src/test/java/com/romanpulov/violetnote/OrderedEntitySortTest.java
+++ app/src/test/java/com/romanpulov/violetnote/OrderedEntitySortTest.java
@@ -60,4 +60,42 @@ public class OrderedEntitySortTest {
         System.out.println(so.getId());
         Assert.assertEquals(so.getId(), 2L);
     }
+
+    @Test
+    public void testPriority() {
+        List<BasicOrderedEntityNoteA> list = new ArrayList<>();
+
+        BasicOrderedEntityNoteA o = new BasicOrderedEntityNoteA();
+        o.setId(1);
+        o.setOrderId(6);
+        o.setPriority(1);
+        list.add(o);
+
+        o = new BasicOrderedEntityNoteA();
+        o.setId(2);
+        o.setOrderId(2);
+        list.add(o);
+
+        o = new BasicOrderedEntityNoteA();
+        o.setId(3);
+        o.setOrderId(4);
+        list.add(o);
+
+        System.out.println("testPriority");
+
+        // higher priority handling
+        BasicOrderedEntityNoteA.sortAsc(list);
+        BasicOrderedEntityNoteA so = list.get(0);
+        System.out.println(so.getId());
+        Assert.assertEquals(so.getId(), 1L);
+
+        // lower priority handling
+        so.setPriority(0);
+        so = list.get(1);
+        long middleId = so.getId();
+        so.setPriority(-1);
+        BasicOrderedEntityNoteA.sortAsc(list);
+        so = list.get(2);
+        Assert.assertEquals(so.getId(), middleId);
+    }
 }
