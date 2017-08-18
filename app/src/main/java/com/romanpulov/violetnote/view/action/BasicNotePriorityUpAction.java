d96a1085d78e5ddd6c5567f5c5ee2820701773c7
MrZeRo2000 <MrZeRo2000@mail.ru>
2017-08-18 12:07:51 +0300

    priority up down for checked items initial implementation

---
+++ app/src/main/java/com/romanpulov/violetnote/view/action/BasicNotePriorityUpAction.java
@@ -0,0 +1,45 @@
+package com.romanpulov.violetnote.view.action;
+
+import com.romanpulov.violetnote.db.DBNoteManager;
+import com.romanpulov.violetnote.model.BasicCommonNoteA;
+import com.romanpulov.violetnote.model.BasicOrderedEntityNoteA;
+import com.romanpulov.violetnote.model.MovementDirection;
+import com.romanpulov.violetnote.view.core.BasicCommonNoteFragment;
+
+import java.util.List;
+
+/**
+ * Created by romanpulov on 18.08.2017.
+ */
+
+public class BasicNotePriorityUpAction<T extends BasicCommonNoteA> extends BasicNoteMoveAction<T> {
+
+    public BasicNotePriorityUpAction(BasicCommonNoteFragment fragment) {
+        super(fragment);
+    }
+
+    @Override
+    public int getDirection() {
+        return MovementDirection.DIRECTION_UP;
+    }
+
+    @Override
+    public boolean execute(DBNoteManager noteManager, T item) {
+        return noteManager.priorityUp(item);
+    }
+
+    @Override
+    public boolean execute(DBNoteManager noteManager, List<T> items) {
+        boolean result = false;
+        BasicOrderedEntityNoteA.sortAsc(items);
+
+        for (T item : items) {
+            if (noteManager.priorityUp(item)) {
+                result = true;
+            } else
+                break;
+        }
+
+        return result;
+    }
+}
