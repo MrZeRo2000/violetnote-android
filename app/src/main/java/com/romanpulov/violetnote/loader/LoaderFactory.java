22932bfc005696da04ff2c50cd4f01c3887b478c
MrZeRo2000 <MrZeRo2000@mail.ru>
2017-11-17 12:33:22 +0200

    Loader small refactoring

---
+++ app/src/main/java/com/romanpulov/violetnote/loader/LoaderFactory.java
@@ -0,0 +1,50 @@
+package com.romanpulov.violetnote.loader;
+
+import android.content.Context;
+
+import com.romanpulov.violetnote.view.preference.PreferenceRepository;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * Factory for DocumentLoader creation
+ * Created by romanpulov on 16.06.2016.
+ */
+public class DocumentLoaderFactory {
+
+    public static Class<? extends AbstractLoader> classFromType(int type) {
+        switch (type) {
+            case PreferenceRepository.SOURCE_TYPE_FILE:
+                return DocumentLocalFileLoader.class;
+            case PreferenceRepository.SOURCE_TYPE_DROPBOX:
+                return DocumentDropboxFileLoader.class;
+            default:
+                return null;
+        }
+    }
+
+    public static AbstractLoader fromClassName(Context context, String className) {
+        Class<?> clazz;
+        try {
+            clazz = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        Constructor<?> contextConstructor;
+        try {
+            contextConstructor = clazz.getConstructor(Context.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        try {
+            return (AbstractLoader) contextConstructor.newInstance(context);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
