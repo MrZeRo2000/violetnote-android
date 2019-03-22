a999aeb1433685043196629eea067bab4bee9319
MrZeRo2000 <MrZeRo2000@mail.ru>
2019-03-22 18:14:29 +0200

    added bold font for note group with max items

--- app/src/main/java/com/romanpulov/violetnote/view/DashboardItemRecyclerViewAdapter.java
+++ app/src/main/java/com/romanpulov/violetnote/view/DashboardItemRecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.romanpulov.violetnote.view;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -20,17 +21,27 @@ public class DashboardItemRecyclerViewAdapter extends RecyclerView.Adapter<Dashb
     private final List<BasicNoteGroupA> mBasicNoteGroupList;
     private final OnBasicGroupInteractionListener mListener;
 
+    //calculated
+    private long mMaxNoteCount = 0;
+
+    private void calcMaxNoteCount() {
+        for (BasicNoteGroupA item : mBasicNoteGroupList) {
+            if (item.getNoteCount() > mMaxNoteCount) {
+                mMaxNoteCount = item.getNoteCount();
+            }
+        }
+    }
+
     public DashboardItemRecyclerViewAdapter(List<BasicNoteGroupA> basicNoteGroupList, OnBasicGroupInteractionListener listener) {
         this.mBasicNoteGroupList = basicNoteGroupList;
         this.mListener = listener;
+        calcMaxNoteCount();
     }
 
     @NonNull
     @Override
     public DashboardItemRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int position) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_dashboard_list_item, parent, false);
-
-
         return new ViewHolder(view);
     }
 
@@ -52,6 +63,9 @@ public class DashboardItemRecyclerViewAdapter extends RecyclerView.Adapter<Dashb
         viewHolder.mItemCountView.setVisibility(noteCount == 0 ? View.GONE : View.VISIBLE);
         if (noteCount > 0) {
             viewHolder.mItemCountView.setText(String.valueOf(noteCount));
+            if (noteCount == mMaxNoteCount) {
+                viewHolder.mItemCountView.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+            }
         }
     }
 
