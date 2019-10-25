0595e7c14b6478e33dfecc9e2b0b49462d39d980
MrZeRo2000 <MrZeRo2000@mail.ru>
2019-10-25 11:59:00 +0300

    tests for new procedures in HNoteCOItems

---
+++ app/src/androidTest/java/com/romanpulov/violetnote/BasicHNoteCOItemDAODBBaseTest.java
@@ -0,0 +1,57 @@
+package com.romanpulov.violetnote;
+
+import com.romanpulov.violetnote.db.dao.BasicHNoteCOItemDAO;
+import com.romanpulov.violetnote.model.BasicHNoteCOItemA;
+import com.romanpulov.violetnote.model.BasicNoteA;
+import com.romanpulov.violetnote.model.BasicNoteGroupA;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.List;
+
+public class BasicHNoteCOItemDAODBBaseTest extends DBBaseTest {
+    @Override
+    void prepareDatabase() {
+        deleteDatabase();
+        (new DataGenerator()).generateData();
+    }
+
+    @Test
+    public void testMain() {
+        synchronized (DBLock.instance) {
+            initDB();
+
+            // get all groups
+            List<BasicNoteGroupA> noteGroups = mDBNoteManager.mBasicNoteGroupDAO.getAll();
+            Assert.assertNotEquals(0, noteGroups.size());
+
+            // get history events group
+            BasicNoteGroupA historyEventsGroup = null;
+            // get history events group
+            for (BasicNoteGroupA group : noteGroups) {
+                if (group.getGroupName().equals("History Events")) {
+                    historyEventsGroup = group;
+                    break;
+                }
+            }
+            Assert.assertNotNull(historyEventsGroup);
+
+            // get animals note
+            List<BasicNoteA> notes = mDBNoteManager.mBasicNoteDAO.getByGroup(historyEventsGroup);
+            Assert.assertNotEquals(0, notes.size());
+
+            BasicNoteA animalsNote = null;
+            for (BasicNoteA note : notes) {
+                if (note.getTitle().equals("Animals")) {
+                    animalsNote = note;
+                    break;
+                }
+            }
+            Assert.assertNotNull(animalsNote);
+
+            List<BasicHNoteCOItemA> hNoteCOItems = mDBHManager.mBasicHNoteCOItemDAO.getByNoteId(animalsNote.getId());
+            Assert.assertEquals(3, hNoteCOItems.size());
+        }
+    }
+}
