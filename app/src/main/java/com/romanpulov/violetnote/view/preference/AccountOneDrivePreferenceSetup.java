816bf1e1936e6bb97b9ff16de22086141f27a11b
MrZeRo2000 <MrZeRo2000@mail.ru>
2019-11-22 14:58:24 +0200

    added ability to select login or logout for onedrive account

--- app/src/main/java/com/romanpulov/violetnote/view/preference/AccountOneDrivePreferenceSetup.java
+++ app/src/main/java/com/romanpulov/violetnote/view/preference/AccountOneDrivePreferenceSetup.java
@@ -1,11 +1,14 @@
 package com.romanpulov.violetnote.view.preference;
 
+import android.content.DialogInterface;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.AlertDialog;
 
 import com.romanpulov.library.dropbox.DropboxHelper;
 import com.romanpulov.violetnote.R;
 import com.romanpulov.violetnote.onedrive.OneDriveHelper;
+import com.romanpulov.violetnote.view.SettingsActivity;
 
 /**
  * Dropbox account configuration
@@ -23,7 +26,23 @@ public class AccountOneDrivePreferenceSetup extends PreferenceSetup {
         mPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                OneDriveHelper.getInstance().createClient(mActivity);
+                final AlertDialog.Builder alert = new AlertDialog.Builder(mActivity, R.style.AlertDialogTheme);
+                alert
+                        .setTitle(R.string.ui_dialog_title_confirmation)
+                        .setPositiveButton(R.string.ui_dialog_button_login, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                OneDriveHelper.getInstance().createClient(mActivity);
+                            }
+                        })
+
+                        .setNegativeButton(R.string.ui_dialog_button_logout, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                OneDriveHelper.getInstance().logout(mActivity);
+                            }
+                        })
+                        .show();
                 return true;
             }
         });
