161285efa5d5f10c317368e3035031cd9acba130
MrZeRo2000 <MrZeRo2020@outlook.com>
2024-11-01 16:10:39 +0200

    iss213 DashboardFragment added settings menu

--- app/src/main/java/com/romanpulov/violetnote/view/DashboardFragment.java
+++ app/src/main/java/com/romanpulov/violetnote/view/DashboardFragment.java
@@ -3,17 +3,19 @@ package com.romanpulov.violetnote.view;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.*;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.MenuHost;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
+import com.romanpulov.violetnote.R;
 import com.romanpulov.violetnote.databinding.FragmentDashboardBinding;
 import com.romanpulov.violetnote.model.BasicNoteGroupA;
 import com.romanpulov.violetnote.model.BasicNoteGroupViewModel;
+import org.jetbrains.annotations.NotNull;
 
 public class DashboardFragment extends Fragment implements OnBasicGroupInteractionListener {
     private FragmentDashboardBinding binding;
@@ -37,6 +39,26 @@ public class DashboardFragment extends Fragment implements OnBasicGroupInteracti
         int gridSpanCount =  orientation == Configuration.ORIENTATION_LANDSCAPE ? 3 : 2;
         binding.list.setLayoutManager(new GridLayoutManager(getContext(), gridSpanCount));
 
+        MenuHost menuHost = requireActivity();
+        menuHost.addMenuProvider(
+                new MenuProvider() {
+                    @Override
+                    public void onCreateMenu(@NotNull Menu menu, @NotNull MenuInflater menuInflater) {
+                        menuInflater.inflate(R.menu.dashboard, menu);
+                    }
+
+                    @Override
+                    public boolean onMenuItemSelected(@NotNull MenuItem menuItem) {
+                        if (menuItem.getItemId() == R.id.action_settings) {
+                            startActivity(new Intent(requireActivity(), SettingsActivity.class));
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+        );
+
         BasicNoteGroupViewModel model = new ViewModelProvider(this).get(BasicNoteGroupViewModel.class);
         model.getAllWithTotals().observe(this, basicNoteGroupList ->
                 binding.list.setAdapter(new DashboardItemRecyclerViewAdapter(basicNoteGroupList, this)));
@@ -52,4 +74,5 @@ public class DashboardFragment extends Fragment implements OnBasicGroupInteracti
             startActivity(intent);
         }
     }
+
 }
