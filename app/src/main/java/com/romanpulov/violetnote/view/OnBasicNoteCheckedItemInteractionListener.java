e1e3a68a069a8d7cdb7781f04cac1b55a7fedbbf
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-01-20 16:59:11 +0200

    Refactoring: removed BasicNoteItemFragment

--- app/src/main/java/com/romanpulov/violetnote/view/OnBasicNoteCheckedItemInteractionListener.java
+++ app/src/main/java/com/romanpulov/violetnote/view/OnBasicNoteCheckedItemInteractionListener.java
@@ -2,6 +2,6 @@ package com.romanpulov.violetnote.view;
 
 import com.romanpulov.violetnote.model.BasicNoteItemA;
 
-public interface OnBasicNoteCheckedItemInteractionListener extends BasicNoteItemFragment.OnBasicNoteItemFragmentInteractionListener {
+public interface OnBasicNoteCheckedItemInteractionListener extends OnBasicNoteItemFragmentInteractionListener {
     void onBasicNoteItemPriceClick(BasicNoteItemA item, int position);
 }
---
+++ app/src/main/java/com/romanpulov/violetnote/view/OnBasicNoteItemFragmentInteractionListener.java
@@ -0,0 +1,7 @@
+package com.romanpulov.violetnote.view;
+
+import com.romanpulov.violetnote.model.BasicNoteItemA;
+
+public interface OnBasicNoteItemFragmentInteractionListener {
+    void onBasicNoteItemFragmentInteraction(BasicNoteItemA item);
+}
