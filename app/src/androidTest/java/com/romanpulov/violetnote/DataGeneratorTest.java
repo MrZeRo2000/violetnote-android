9962c7d31477780557aef83b6669cc13353ce287
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-01-28 10:38:08 +0200

    Refactoring: remove unused

--- app/src/androidTest/java/com/romanpulov/violetnote/DataGeneratorTest.java
+++ app/src/androidTest/java/com/romanpulov/violetnote/DataGeneratorTest.java
@@ -1,27 +1,12 @@
 package com.romanpulov.violetnote;
 
 import androidx.test.filters.SmallTest;
-import android.util.Log;
 
 import org.junit.*;
 
-import static org.junit.Assert.assertEquals;
-
-//@Ignore
+@Ignore("Only needed on demand")
 @SmallTest
 public class DataGeneratorTest {
-    private final static String TAG = "DataGeneratorTest";
-
-    private static void log(String message) {
-        Log.d(TAG, message);
-    }
-
-    @Test
-    public void test1() {
-        log("Data Generator test message");
-        assertEquals(1, 1);
-    }
-
     @Test
     public void generateData() {
         DataGenerator d = new DataGenerator();
